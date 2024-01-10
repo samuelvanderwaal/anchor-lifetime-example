648fa87af2cde5a6ad57a689bb18b565c465928c
Samuel Vanderwaal <samuel.vanderwaal@magiceden.io>
2024-01-10 12:12:52 -0900

    using lifetimes in anchor example

--- programs/lifetime/src/lib.rs
+++ programs/lifetime/src/lib.rs
@@ -6,8 +6,14 @@ declare_id!("52XbisxVLPiSTV7voRXMkRjX31yw9XwZJJPQSi81tVkV");
 pub mod lifetime {
     use super::*;
 
-    pub fn do_thing<'info>(ctx: Context<'_, 'info, 'info, '_, DoThing<'info>>) -> Result<()> {
-        handle(ctx)
+    pub fn do_thing<'c: 'info, 'info>(
+        ctx: Context<'_, 'c, 'info, 'info, DoThing<'info>>,
+    ) -> Result<()> {
+        let account1 = Account::<Account1>::try_from(&ctx.accounts.account1)?;
+
+        msg!("other_account.value: {}", account1.value);
+
+        Ok(())
     }
 }
 
@@ -15,6 +21,11 @@ pub mod lifetime {
 #[derive(Default)]
 pub struct Account1 {
     pub value: String,
+}
+
+#[derive(Accounts)]
+pub struct DoThing<'info> {
+    pub account1: UncheckedAccount<'info>,
 }
 
 // #[derive(Accounts)]
@@ -29,16 +40,3 @@ pub struct Account1 {
 
 //     Ok(())
 // }
-
-#[derive(Accounts)]
-pub struct DoThing<'info> {
-    pub account1: UncheckedAccount<'info>,
-}
-
-pub fn handle<'info>(ctx: Context<'_, 'info, 'info, '_, DoThing<'info>>) -> Result<()> {
-    let account1 = Account::<Account1>::try_from(&ctx.accounts.account1)?;
-
-    msg!("other_account.value: {}", account1.value);
-
-    Ok(())
-}
