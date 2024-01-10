83c9e9625c7013707987a69d799b1c5fad1d6773
Samuel Vanderwaal <samuel.vanderwaal@magiceden.io>
2024-01-10 12:07:54 -0900

    lifetime issue w/ try_from

--- programs/lifetime/src/lib.rs
+++ programs/lifetime/src/lib.rs
@@ -17,28 +17,28 @@ pub struct Account1 {
     pub value: String,
 }
 
-#[derive(Accounts)]
-pub struct DoThing<'info> {
-    pub account1: Account<'info, Account1>,
-}
-
-pub fn handle(ctx: Context<DoThing>) -> Result<()> {
-    let account1 = &ctx.accounts.account1;
-
-    msg!("other_account.value: {}", account1.value);
-
-    Ok(())
-}
-
 // #[derive(Accounts)]
 // pub struct DoThing<'info> {
-//     pub account1: UncheckedAccount<'info>,
+//     pub account1: Account<'info, Account1>,
 // }
 
-// pub fn handle<'info>(ctx: Context<'_, 'info, 'info, '_, DoThing<'info>>) -> Result<()> {
-//     let account1 = Account::<Account1>::try_from(&ctx.accounts.account1)?;
+// pub fn handle(ctx: Context<DoThing>) -> Result<()> {
+//     let account1 = &ctx.accounts.account1;
 
 //     msg!("other_account.value: {}", account1.value);
 
 //     Ok(())
 // }
+
+#[derive(Accounts)]
+pub struct DoThing<'info> {
+    pub account1: UncheckedAccount<'info>,
+}
+
+pub fn handle(ctx: Context<DoThing>) -> Result<()> {
+    let account1 = Account::<Account1>::try_from(&ctx.accounts.account1)?;
+
+    msg!("other_account.value: {}", account1.value);
+
+    Ok(())
+}
