414dc89f276658d45d749a327d0b61d9083584fa
Samuel Vanderwaal <samuel.vanderwaal@magiceden.io>
2024-01-10 12:06:28 -0900

    boilerplate--working

---
+++ programs/lifetime/src/lib.rs
@@ -0,0 +1,44 @@
+use anchor_lang::prelude::*;
+
+declare_id!("52XbisxVLPiSTV7voRXMkRjX31yw9XwZJJPQSi81tVkV");
+
+#[program]
+pub mod lifetime {
+    use super::*;
+
+    pub fn do_thing(ctx: Context<DoThing>) -> Result<()> {
+        handle(ctx)
+    }
+}
+
+#[account]
+#[derive(Default)]
+pub struct Account1 {
+    pub value: String,
+}
+
+#[derive(Accounts)]
+pub struct DoThing<'info> {
+    pub account1: Account<'info, Account1>,
+}
+
+pub fn handle(ctx: Context<DoThing>) -> Result<()> {
+    let account1 = &ctx.accounts.account1;
+
+    msg!("other_account.value: {}", account1.value);
+
+    Ok(())
+}
+
+// #[derive(Accounts)]
+// pub struct DoThing<'info> {
+//     pub account1: UncheckedAccount<'info>,
+// }
+
+// pub fn handle<'info>(ctx: Context<'_, 'info, 'info, '_, DoThing<'info>>) -> Result<()> {
+//     let account1 = Account::<Account1>::try_from(&ctx.accounts.account1)?;
+
+//     msg!("other_account.value: {}", account1.value);
+
+//     Ok(())
+// }
